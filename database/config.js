00f5d5518619f6de92d42598d8dac70a3db62a8e
김동원 <dongwon0103@naver.com>
2022-07-29 00:10:07 +0900

    db connection test

--- database/config.js
+++ database/config.js
@@ -1,1 +1,20 @@
 const dotenv = require("dotenv");
+
+dotenv.config();
+
+const config = {
+    development: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'project_a'
+    },
+    test: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'project_a_test'
+    }
+  };
+
+  module.exports = config;
---
+++ database/connect.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+const config = require('./config');
+dotenv.config();
+
+// 현재 test 연결
+const conn = mysql.createConnection(
+    config['test']  
+);
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log("connect success!");
+});
+
+conn.end();
