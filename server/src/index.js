1bb9bee4ea6c7704d44107fab0f0f119cdd2fc4c
EastWon0103 <dongwon000103@gmail.com>
2023-03-14 22:42:54 +0900

    delete: swagger index.js

--- server/src/index.js
+++ server/src/index.js
@@ -10,9 +10,6 @@ const ProductRoute = require("./routes/product_routes.js");
 var bodyParser = require("body-parser");
 
 var cookieParser = require('cookie-parser');
-var swaggerJsdoc = require("swagger-jsdoc");
-var swaggerUi = require("swagger-ui-express");
-const swaggerOption = require("./swagger/swagger.js")
 const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config({ path: path.join(__dirname, "/./../enviroment/.env") });
@@ -39,11 +36,5 @@ passportConfig();
 
 app.use("/api/user", UserRoute);
 app.use("/api/product", ProductRoute);
-
-const specs = swaggerJsdoc(swaggerOption);
-app.use("/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs, { explorer: true })
-);
 
 app.listen(process.env.TEST_SERVER_PORT, () => console.log("server start"));
