29d94f9a1b2be06312f502e6dadfb6f15d326774
EastWon0103 <dongwon000103@gmail.com>
2023-03-14 23:17:57 +0900

    add: 이메일 validation

--- server/src/controller/user_controller.js
+++ server/src/controller/user_controller.js
@@ -1,5 +1,9 @@
 const User = require("../model/user_model.js");
 const { Encryption, Decryption } = require("../utils/crypto-util/crypto_util.js");
+const validEmailCheck = (email) =>{
+    const pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    return pattern.test(email)
+}
 
 exports.findByEmail = function (req, res) {
     User.findByEmail(req.params.email, (err, user) => {
@@ -17,7 +21,16 @@ exports.findByEmail = function (req, res) {
 
 exports.userSignUp = async function (req, res) {
     const { nickname, password, email, picture } = req.body;
-    //이메일 형식이 맞는지 체크 해야함
+    
+    if(!validEmailCheck(email)) {
+        res.status(400).send({
+            success: false,
+            message: "이메일 형식이 올바르지 않습니다."
+        });
+        return;
+    }
+
+    
     const hashedPassword = await Encryption(password);
     const USER_ROLE = 1;
     User.create(nickname, hashedPassword, email, picture, USER_ROLE, (err, user) => {
