5bbe25ea60a72833a61484d2e82a5da2a61fc14a
EastWon0103 <dongwon000103@gmail.com>
2023-04-13 03:08:24 +0900

    fix: datetime -> string 출력(mysql2 KST 적용안됨 해결)

--- server/database/config.js
+++ server/database/config.js
@@ -9,7 +9,11 @@ const config = {
         password: process.env.DB_PASS,
         port: process.env.DB_PORT,
         database: process.env.DB_NAME,
-        charset: "utf8mb4"
+        charset: "utf8mb4",
+        dateStrings: [ 
+            'DATE', 
+            'DATETIME' 
+        ]
     },
     dev: {
         host: "dev_db_container",
@@ -17,8 +21,13 @@ const config = {
         password: process.env.DB_PASS,
         port: process.env.DB_PORT,
         database: process.env.DB_TEST_NAME,
-        charset: "utf8mb4"
+        charset: "utf8mb4",
+        dateStrings: [ 
+            'DATE', 
+            'DATETIME' 
+        ]
     }
 };
+// dateStrings Date에서 String으로 출력: mysql2에서 timezone 세팅이 안됨
 
 module.exports = config;
