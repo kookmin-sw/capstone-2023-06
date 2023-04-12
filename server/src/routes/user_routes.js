81966459b7af011a82fe5b92eda6ecac68991fe4
EastWon0103 <dongwon000103@gmail.com>
2023-04-12 19:28:33 +0900

    add: ImageUploader

--- server/src/routes/user_routes.js
+++ server/src/routes/user_routes.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const passport = require("passport");
-var router = express.Router();
-var userController = require("../controller/user_controller.js");
+const router = express.Router();
+const userController = require("../controller/user_controller.js");
+const ImageUploader = require("../utils/imageUploader/imageUploader.js");
 
 router.post("/sign-up", userController.userSignUp);
 router.post("/login", passport.authenticate('local'), userController.login);
 router.get("/auto-login", userController.autologin);
 router.get("/logout", userController.logout);
-router.get("/:email", userController.findByEmail);
-
+router.get("/id/:id", userController.findById)
+router.get("/email/:email", userController.findByEmail);
+router.post("/profile", ImageUploader.single("image"), userController.uploadProfile);
+router.delete("/", userController.deleteUser);
 
 module.exports = router;
 
