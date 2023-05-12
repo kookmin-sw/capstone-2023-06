7026b2f02eaf674c39989940de9d6dfb7b6f61d6
EastWon0103 <dongwon000103@gmail.com>
2023-05-12 15:51:54 +0900

    add: NODE_PATH

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SERVER_ROOT=/home/ubuntu/desk-it/server
+NODE_PATH=/home/ubuntu/.nvm/versions/node/v19.4.0/bin
 
 cd $SERVER_ROOT
-sudo pm2 kill
-sudo npm run prod
+sudo $NODE_PATH/pm2 kill
+sudo $NODE_PATH/npm run prod
