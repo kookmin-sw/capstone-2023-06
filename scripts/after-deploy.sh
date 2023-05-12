877c89d9daf2622e5ba64c663cc708de626fdb7d
EastWon0103 <dongwon000103@gmail.com>
2023-05-12 16:10:12 +0900

    add: NVM dir

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 SERVER_ROOT=/home/ubuntu/desk-it/server
 
 cd $SERVER_ROOT
