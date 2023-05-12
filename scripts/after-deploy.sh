466ca1c0e21a4eaf826f346c3121d8fd381d03d1
EastWon0103 <dongwon000103@gmail.com>
2023-05-12 15:58:53 +0900

    add: pwd for trace

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SERVER_ROOT=/home/ubuntu/desk-it/server
-NODE_PATH=/home/ubuntu/.nvm/versions/node/v19.4.0/bin
 
 cd $SERVER_ROOT
-sudo $NODE_PATH/pm2 kill
-sudo $NODE_PATH/npm run prod
+pwd
+pm2 kill
+npm run prod
