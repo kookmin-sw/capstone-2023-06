d78f8c5435cfed438ca10e4a5d99d95f2215fc4a
EastWon0103 <dongwon000103@gmail.com>
2023-05-15 03:39:12 +0900

    fix: 소소한 수정

--- server/src/routes/passport.js
+++ server/src/routes/passport.js
@@ -8,15 +8,12 @@ const { Decryption } = require("../utils/crypto-util/crypto_util.js");
 module.exports = () => {
     // Strategy 성공 시 호출됨
     passport.serializeUser((user, done) => {
-        console.log('serr ', user.email);
         done(null, user); // 여기의 user가 deserializeUser의 첫 번째 매개변수로 이동
     });
 
     // 페이지를 방문할때마다 세션스토어에서 가져와서
     passport.deserializeUser((user, done) => {
         // 매개변수 user는 serializeUser의 done의 인자 user를 받은 것
-        console.log('deserr ', user.email);
-
         done(null, user);   // 여기의 user가 req.user가 됨
     });
 
